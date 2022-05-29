13e29b00f2cae690671ee27cfc9a21cf3b8ea9cd
Fredfed-89 <fedyaev.sergei89@gmail.com>
2022-05-29 09:57:40 +0300

    first commit

---
+++ src/js/components/sliders.js
@@ -0,0 +1,19 @@
+import Swiper from 'swiper/bundle';
+
+const mySwiper = new Swiper('.swiper', {
+  // modules: [Navigation, Pagination],
+  slidesPerView: 1,
+  // spaceBetween: 30,
+  // loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'custom',
+    clickable: true,
+  },
+});
